a01b8801f0f1932a8645975cc2bbc30f2383a741
SrAxel <Thesense@outlook.es>
2024-12-19 14:19:44 -0300

    #

--- Models/Sale.cs
+++ Models/Sale.cs
@@ -3,15 +3,21 @@
     public class Sale
     {
         public int Id { get; set; }
-        public string PaymentMethod { get; set; } = string.Empty;
         public int CarId { get; set; }
         public int CarPrice { get; set; }
-        public string Make { get; set; }
-        public string Model { get; set; }
         public Car? Car { get; set; }
         public DateOnly SaleDate { get; set; }
         public string ClientId { get; set; } = string.Empty;
-        public string Name_or_CompanyName { get; set; }
         public Client? Client { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+    }
+    public enum PaymentMethod
+    {
+        Efectivo,
+        Transferencia,
+        CreditoPrendario,
+        CreditoBancario,
+        TarjetaDeCredito,
+        Financiación
     }
 }
